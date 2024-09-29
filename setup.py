c6091b290fccf66b35c9f1a83dfe4afb63d6e7f6
Eli Temanson <temanson.eli@gmail.com>
2024-09-29 12:58:43 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,58 @@
+import urllib.request
+
+urllib.request.urlretrieve('http://www.example.com/songs/mp3.mp3', 'mp3.mp3')
+links = [ 
+    'https://www.lp2ib.in2p3.fr/wp-content/uploads/2024/01/GEF-Full-Y2023-3-2.zip',
+    ''
+]
+gitdir = [
+    'https://github.com/precice/precice.git',
+    'https://github.com/eli-temanson/AZURE2-qt5.git',
+    'https://github.com/I-Thompson/fresco.git',
+    'https://github.com/alvolya/cosmo.git',
+    'https://github.com/lanl/CGMF.git',
+    'https://github.com/hrosiak/catima.git',
+    'https://gitlab.in2p3.fr/np/nptool',
+]
+misc = [
+    'https://sites.google.com/alumni.tsukuba.ac.jp/kshell-nuclear/home?authuser=0',
+    'https://lise.frib.msu.edu/download/macOS/LISE++-macos-installer-universal-17.7.1.pkg',
+    'https://lise.frib.msu.edu/download/Linux/lise-app_v17.7.run'
+]
+
+
+# using nix: sh <(curl -L https://nixos.org/nix/install)
+# ---- dev tools
+# vscode
+# premake, cmake
+# texstudio, MikTex / typst
+# ---- install languages
+# go
+# rust: curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# flutter
+# ---- useful c++ libraries
+# root
+# geant4
+# libtorch
+# gsl
+# fftw
+# eigen
+# asio
+
+
+
+# ---- calc codes
+# AZURE2
+# FRESCO
+# COSMO
+# GEF
+# CGMF
+# 
+# catima
+# nptool
+# 
+# ----- Apps 
+# firefox
+# Zotero
+# 
+
